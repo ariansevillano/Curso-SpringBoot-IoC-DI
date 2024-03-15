c2ae4f0ae726e0717d31bff71f68a5d2b09c2af6
ariansevillano <ariansevillano1@gmail.com>
2024-03-15 10:57:48 -0500

    Creación del POJO en model y del repository con el findAll

---
+++ src/main/java/com/arian/springboot/di/app/springbootdi/repositories/ProductRepository.java
@@ -0,0 +1,29 @@
+package com.arian.springboot.di.app.springbootdi.repositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.arian.springboot.di.app.springbootdi.models.Product;
+
+public class ProductRepository {
+    
+    private List<Product> data;
+
+    public ProductRepository() {
+        this.data = Arrays.asList(
+            new Product(1L, "Arian", 150L),
+            new Product(2L, "Erick", 180L),
+            new Product(3L, "Juan", 190L)
+        );
+    }
+
+    public List<Product> findAll() {
+
+        return data;
+    }
+
+
+
+
+
+}
